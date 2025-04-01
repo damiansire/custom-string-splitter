6de384559813eadeaef2804ef1b66d47b7d8f6dc
Damian Sire <damiansire@gmail.com>
2025-04-01 14:05:26 +0700

    feat: add case to handle custom rules with break characters in strings
    This commit introduces a new case to manage custom rules that include break characters, specifically spaces, within string values like "in progress". This ensures that the parser correctly interprets and handles these scenarios, preventing potential errors or misinterpretations.

--- libs/get-next-word/get-next-word.ts
+++ libs/get-next-word/get-next-word.ts
@@ -1,14 +1,11 @@
 import { CustomSplitOptions, IterationData } from "../../types";
-
+import { fromRuleToSet } from "../from-rules-to-set/from-rules-to-set";
 export const getNextWord = (rawData: string, options: CustomSplitOptions, currentIndex: number): IterationData => {
-  const {
-    breakCharacters = [],
-    breakWords = [],
-    ignoreCharacters = [],
-    ignoreWords = [],
-    customRules: [],
-  } = options;
+  const { breakCharacters = [], breakWords = [], ignoreCharacters = [], ignoreWords = [], customRules = [] } = options;
+
   let currentWord = "";
+
+  const rulesSet = fromRuleToSet(customRules);
 
   while (ignoreCharacters.includes(rawData[currentIndex])) {
     currentIndex++;
@@ -20,7 +17,20 @@ export const getNextWord = (rawData: string, options: CustomSplitOptions, curren
     return { word: currentWord, endWordIndex: currentIndex - 1 };
   }
 
-  while (currentIndex < rawData.length && !breakCharacters.includes(rawData[currentIndex])) {
+  let ruleActive: Set<string> | null = null;
+
+  while (currentIndex < rawData.length && (!breakCharacters.includes(rawData[currentIndex]) || ruleActive)) {
+    const token = rawData[currentIndex];
+    if (ruleActive) {
+      if (ruleActive.has(token)) {
+        ruleActive = null;
+      }
+    } else {
+      if (token in rulesSet) {
+        ruleActive = rulesSet[token];
+      }
+    }
+
     currentWord += rawData[currentIndex];
     currentIndex++;
   }
